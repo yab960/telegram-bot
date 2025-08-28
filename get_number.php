3f81fc521d7349d2f935fe09c986cbcbd3e2e216
yab960 <yabsrasherif01786@gmail.com>
2025-08-28 18:37:04 +0300

    Add files via upload

--- get_number.php
+++ get_number.php
@@ -16,6 +16,7 @@ $db_pass=getenv('db_pass');
 
 if (isset($data['message'])) {
     $chat_id = $data['message']['chat']['id'];
+    $first_name=$data['message']['from']['first_name']
     if (isset($data['message']['text']) && strcasecmp($data['message']['text'], '/start') === 0) {
         $keyboard = [
             'keyboard' => [[['text' => 'Share Phone Number', 'request_contact' => true]]],
@@ -26,7 +27,7 @@ if (isset($data['message'])) {
     }
     if (isset($data['message']['contact'])) {
         $phone = $data['message']['contact']['phone_number'];        
-        store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone);
+        store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone,$first_name);
 
         
     }
@@ -50,16 +51,16 @@ function sendMessage($chat_id, $text, $keyboard = null) {
     return json_decode($result, true);
 }
 
-function store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone){
+function store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone,$first_name){
 
         try{
 
             $conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
             $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-            $stmt =$conn->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
-            $stmt->execute([':name'=>$phone,':email'=>$chat_id]);
+            $stmt =$conn->prepare("INSERT INTO users (phone_number, chat_id,first_name) VALUES (:phone_number, :chat_id, :first_name)");
+            $stmt->execute([':phone_number'=>$phone,':chat_id'=>$chat_id,':first_name'=>$first_name]);
 
-            sendMessage($chat_id, "Thanks! Your number is: $phone");
+            sendMessage($chat_id, "Dear! $first_name Your number is: $phone You Have Sucessfully Registered On Bingo Bay");
         }
         catch (PDOException $e){
             sendMessage($chat_id, "Error:"  . $e->getMessage());
