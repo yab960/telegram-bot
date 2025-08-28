6fd972c6a02443e15ad45e9d2c3fff1842792e81
yab960 <yabsrasherif01786@gmail.com>
2025-08-28 13:58:27 +0300

    Add files via upload

--- get_number.php
+++ get_number.php
@@ -8,6 +8,12 @@ error_reporting(E_ALL);
 $input = file_get_contents('php://input');
 error_log("Webhook triggered. Input: $input");
 $data = json_decode($input, true);
+$db_host='dpg-d2ncplvdiees73cg2l00-a';
+$db_port='5432';
+$db_name='bingodb_ln7t';
+$db_user='bingodb_ln7t_user';
+$db_pass='REUKeK5sT9mzpYPzwMXt0qJBlBrvoTr4';
+
 
 
 if (isset($data['message'])) {
@@ -18,13 +24,22 @@ if (isset($data['message'])) {
             'resize_keyboard' => true,
             'one_time_keyboard' => true
         ];
-        $esponse = sendMessage($chat_id, "Please share your phone number:", $keyboard);
+        sendMessage($chat_id, "Please share your phone number:", $keyboard);
     }
     if (isset($data['message']['contact'])) {
-        $phone = $data['message']['contact']['phone_number'];
-        
+        $phone = $data['message']['contact']['phone_number'];        
 
-        
+        try{
+            $conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+            $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+            $stmt =$conn->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
+            $stmt->execute([':name'=>'yab',':email'=>'email']);
+
+            sendMessage($chat_id, "Thanks! Your number is: $phone");
+        }
+        catch (PDOException $e){
+            sendMessage($chat_id, "Error:"  . $e->getMessage());
+        }
         
     }
 }
