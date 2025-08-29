8c49e2ebcae26f0dc8fac15b3c108dd29aead239
yab960 <yabsrasherif01786@gmail.com>
2025-08-29 12:28:06 +0300

    Add files via upload

---
+++ check_user.php
@@ -0,0 +1,25 @@
+<?php
+
+$data = json_decode(file_get_contents('php://input'), true);
+$chat_id = $data['chat_id'] ?? null;
+$db_host=getenv('db_host');
+$db_port=getenv('db_port');
+$db_name=getenv('db_name');
+$db_user=getenv('db_user');
+$db_pass=getenv('db_pass');
+
+$conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+$conn ->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+$response = ['registered' => false];
+
+if($chat_id){
+    $stmt=$conn->prepare("SELECT first_name FROM users WHERE chat_id = :chat_id");
+    $stmt ->execute([':chat_id'=>$chat_id]);
+    if($stmt->fetch()){
+
+        $response['registered'] = true;
+    }
+}
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
