1a7f2f8390a42c5e93793ca2f33349aa8c78bf7e
yab960 <yabsrasherif01786@gmail.com>
2025-08-29 12:24:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bingo home</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <script src="scripts/authentication.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>working</h1>
+    <div id="data"></div>
+    
+</body>
+</html>
