c75e09896dbcb4d9cbd8fbcfc86395b2802a61bb
yab960 <yabsrasherif01786@gmail.com>
2025-08-29 08:51:01 +0300

    Add files via upload

--- get_number.php
+++ get_number.php
@@ -14,6 +14,9 @@ $db_name=getenv('db_name');
 $db_user=getenv('db_user');
 $db_pass=getenv('db_pass');
 
+$conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+$conn ->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+
 if (isset($data['message'])) {
     $chat_id = $data['message']['chat']['id'];
     $first_name=$data['message']['from']['first_name'];
@@ -27,7 +30,7 @@ if (isset($data['message'])) {
     }
     if (isset($data['message']['contact'])) {
         $phone = $data['message']['contact']['phone_number'];        
-        store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone,$first_name);
+        store_in_db($conn,$chat_id,$phone,$first_name);
 
         
     }
@@ -51,12 +54,11 @@ function sendMessage($chat_id, $text, $keyboard = null) {
     return json_decode($result, true);
 }
 
-function store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phone,$first_name){
+function store_in_db($conn,$chat_id,$phone,$first_name){
 
         try{
 
-            $conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
-            $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+
             $stmt =$conn->prepare("INSERT INTO users (phone_number, chat_id,first_name) VALUES (:phone_number, :chat_id, :first_name)");
             $stmt->execute([':phone_number'=>$phone,':chat_id'=>$chat_id,':first_name'=>$first_name]);
 
@@ -66,4 +68,9 @@ function store_in_db($db_host,$db_port,$db_name,$db_user,$db_pass,$chat_id,$phon
             sendMessage($chat_id, "Error:"  . $e->getMessage());
         }
 }
+
+function check_user(){
+
+}
+
 ?>
