d691fb230ceaab65104668d694cc1d593e77c048
yab960 <yabsrasherif01786@gmail.com>
2025-08-29 11:14:05 +0300

    Add files via upload

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,2 +1,2 @@
 const user= Telegram.WebApp.initDataUnsafe.user;
-document.getElementById('data').innerHTML=<pre>${JSON.stringify(user,null,2)}</pre>
+document.getElementById('data').innerHTML=`<pre>${JSON.stringify(user,null,2)}</pre>`
